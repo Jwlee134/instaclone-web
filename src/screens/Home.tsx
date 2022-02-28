2036340e35066cee57c04a6bbe3bcd822873f36b
Jaewon <sorhd134@gmail.com>
2022-02-28 12:55:58 +0900

    Reactive Variables

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,7 @@
+import { isLoggedInVar } from "../apollo";
+
 function Home() {
-  return <h1>Home</h1>;
+  return <h1 onClick={() => isLoggedInVar(false)}>Home</h1>;
 }
 
 export default Home;
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,5 +1,7 @@
+import { isLoggedInVar } from "../apollo";
+
 function Login() {
-  return <h1>Login</h1>;
+  return <h1 onClick={() => isLoggedInVar(true)}>Login</h1>;
 }
 
 export default Login;
