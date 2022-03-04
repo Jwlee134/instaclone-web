4cd6e31de98421caeee821750df77c84a872a091
Jaewon <sorhd134@gmail.com>
2022-03-04 17:17:15 +0900

    Parsing Hashtags part One

--- src/components/feed/Comment.tsx
+++ src/components/feed/Comment.tsx
@@ -1,10 +1,17 @@
 import styled from "styled-components";
 import { FatText } from "../shared";
+import sanitizeHtml from "sanitize-html";
 
 const CommentContainer = styled.div``;
 
 const CommentCaption = styled.span`
   margin-left: 10px;
+  mark {
+    background-color: inherit;
+    color: ${({ theme }) => theme.palette.blue};
+    text-decoration: underline;
+    cursor: pointer;
+  }
 `;
 
 interface Props {
@@ -16,7 +23,14 @@ function Comment({ username, text }: Props) {
   return (
     <CommentContainer>
       <FatText>{username}</FatText>
-      <CommentCaption>{text}</CommentCaption>
+      <CommentCaption
+        dangerouslySetInnerHTML={{
+          __html: sanitizeHtml(
+            text?.replace(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g, "<mark>$&</mark>")!,
+            { allowedTags: ["mark"] }
+          ),
+        }}
+      />
     </CommentContainer>
   );
 }
